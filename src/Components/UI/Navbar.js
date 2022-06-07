dc4cfa6424fcdf1fdffccdafd9b8c240056956c5
MarcinMaster <m.luba505@gmail.com>
2022-06-07 07:03:51 +0200

    Added a pretty Navbar

---
+++ src/Components/UI/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+    return (<div className='navbar'>
+        <p className="nav-link">About</p>
+        <p  className="nav-link">Contact</p>
+        <p  className="nav-link">History</p>
+
+    </div>)
+}
+
+export default Navbar;
